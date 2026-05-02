067491f27c94b81841f41417eee45ac0e164562e
SomewhatMay <umayeer.ahsan@hotmail.com>
2026-05-02 00:52:52 -0400

    support coverage in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    setupFiles: ["test/setup.ts"],
     environment: "node",
     include: ["test/**/*.test.ts"],
-    setupFiles: ["test/setup.ts"],
     globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov", "html"],
+
+      include: ["src/**/*.ts"],
+
+      exclude: ["src/index.ts"],
+    },
   },
 });
